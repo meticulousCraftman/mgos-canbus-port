1d66293e90f88cddbff4bae00a1c0c14edd0bf34
Apoorva Singh <apoorvasingh157@gmail.com>
2020-08-12 14:26:58 +0530

    Housekeeping.

--- src/main.c
+++ src/main.c
@@ -1,13 +1,8 @@
 #include "mgos.h"
 
-static void timer_cb(void *arg)
-{
-    // Write your code here
-    (void)arg;
-}
 
 enum mgos_app_init_result mgos_app_init(void)
 {
-    mgos_set_timer(1000 /* ms */, MGOS_TIMER_REPEAT, timer_cb, NULL);
+    LOG(LL_INFO, ("Hello, world!"));
     return MGOS_APP_INIT_SUCCESS;
 }
