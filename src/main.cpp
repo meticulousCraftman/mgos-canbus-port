f35edaada2bebaf338ffda244e58fda4a5fa77eb
Apoorva Singh <apoorvasingh157@gmail.com>
2020-08-13 00:48:29 +0530

    Renamed files

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include "mgos.h"
-#include "sd_try.h"
+#include "tcu_sdcard.h"
 
 
-
+extern "C" {
     enum mgos_app_init_result mgos_app_init(void)
     {
         LOG(LL_INFO, ("Setting up SD Card!"));
         setup_sdcard();
         return MGOS_APP_INIT_SUCCESS;
     }
+}
